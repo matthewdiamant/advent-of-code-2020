936550b30673538e9eb59390e531d40fe9f204dc
Matt Diamant <matthewdiamant@gmail.com>
2021-11-30 09:57:33 -0600

    day 17 cleanup, use set over array, no trimming

--- day-17/day-17.py
+++ day-17/day-17.py
@@ -24,55 +24,39 @@ def neighbors(key, dimensions):
     return directions_filtered
 
 def parse_input_to_grid(input, dimensions):
-    grid = {}
+    grid = set()
     lines = input.split('\n')
     for y, line in enumerate(lines):
         for x, val in enumerate(line):
             key = (x, y, 0, 0) if dimensions == 4 else (x, y, 0)
-            grid[key] = val
+            if val == "#":
+                grid.add(key)
     return grid
 
 def candidates(grid, dimensions):
-    return unique(flatten([neighbors(key, dimensions) for key in grid.keys()]))
+    return unique(flatten([neighbors(key, dimensions) for key in grid]))
 
 def active(grid, key, dimensions):
     candidate_neighbors = neighbors(key, dimensions)
     active_neighbors = 0
     for neighbor in candidate_neighbors:
-        neighbor_active = grid[neighbor] == "#" if grid.has_key(neighbor) else False
+        neighbor_active = neighbor in grid
         active_neighbors += 1 if neighbor_active else 0
-    active = grid[key] == "#" if grid.has_key(key) else False
+    active = key in grid
     new_active = True if (active and (active_neighbors == 2)) or active_neighbors == 3 else False
     return new_active
-
-def trim_side(grid, axis, direction):
-    bound = get_bounds(grid, axis)[direction];
-    face = filter(lambda c: int(c[axis]) == bound, grid.keys())
-    while (len(filter(lambda c: grid[c] == '#', face)) == 0):
-        for c in face:
-            grid.pop(c, None)
-        bound = get_bounds(grid, axis)[direction]
-        face = filter(lambda c: int(c[axis]) == bound, grid.keys())
-
-def trim_grid(grid):
-    trim_side(grid, 0, 0)
-    trim_side(grid, 0, 1)
-    trim_side(grid, 1, 0)
-    trim_side(grid, 1, 1)
-    trim_side(grid, 2, 0)
-    trim_side(grid, 2, 1)
-    return grid
 
 def epoch(grid, dimensions):
     new_candidates = candidates(grid, dimensions)
     new_grid = {}
     for key in new_candidates:
         candidate_active = active(grid, key, dimensions)
-        new_grid[key] = '#' if candidate_active else '.'
+        if candidate_active:
+            new_grid[key] = '#'
     return new_grid
 
 def get_bounds(grid, x):
-    bounds = sorted(map(lambda key: key[x], grid.keys()))
+    bounds = sorted(map(lambda key: key[x], grid))
     return [bounds[0], bounds[-1]]
 
 def grid_to_string(grid):
@@ -85,7 +69,7 @@ def grid_to_string(grid):
         for y in range(low_y_bound, high_y_bound + 1):
             line_output = ""
             for x in range(low_x_bound, high_x_bound + 1):
-                line_output += grid[(x, y, z)]
+                line_output += "#" if (x, y, z) in grid else '.'
             grid_output += line_output + '\n'
         grid_output += '\n'
     return grid_output
@@ -94,7 +78,6 @@ def epochs(grid, times, dimensions):
     new_grid = grid
     for _ in range(times):
         new_grid = epoch(new_grid, dimensions)
-        new_grid = trim_grid(new_grid)
     return new_grid
 
 def count_active(grid):
@@ -123,7 +106,7 @@ sample = """.#.
 ###
 """
 
-sample_grid = {(1, 1, 0): '.', (2, 1, 0): '#', (0, 2, 0): '#', (1, 0, 0): '#', (2, 2, 0): '#', (1, 2, 0): '#', (0, 0, 0): '.', (0, 1, 0): '.', (2, 0, 0): '.'}
+sample_grid = set([(1, 0, 0), (0, 2, 0), (1, 2, 0), (2, 1, 0), (2, 2, 0)])
 
 sample_neighbors = [(0, 1, 2), (0, 1, 3), (0, 1, 4), (0, 2, 2), (0, 2, 3), (0, 2, 4), (0, 3, 2), (0, 3, 3), (0, 3, 4), (1, 1, 2), (1, 1, 3), (1, 1, 4), (1, 2, 2), (1, 2, 4), (1, 3, 2), (1, 3, 3), (1, 3, 4), (2, 1, 2), (2, 1, 3), (2, 1, 4), (2, 2, 2), (2, 2, 3), (2, 2, 4), (2, 3, 2), (2, 3, 3), (2, 3, 4)]
 
